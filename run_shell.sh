c9412e17a0b336bca2200ba4e7ce267a4a6f4957
MaziyarFG <maziyar.felegari@gmail.com>
2023-05-07 11:53:14 +0330

    edit_dir

--- run_shell.sh
+++ run_shell.sh
@@ -4,8 +4,8 @@ sudo apt install tmux
 
 mkdir ~/.tmux_conf
 
-git clone https://github.com/tmux-plugins/tmux-battery.git ~/.tmux_conf
-git clone https://github.com/tmux-plugins/tmux-resurrect.git ~/.tmux_conf
-git clone https://github.com/tmux-plugins/tmux-cpu.git ~/.tmux_conf
-git clone https://github.com/tmux-plugins/tmux-sidebar.git ~/.tmux_conf
-git clone https://github.com/MaziyarFG/tmux_config.git ~/.tmux_conf
+git clone https://github.com/tmux-plugins/tmux-battery.git ~/.tmux_conf/tmux-battery	
+git clone https://github.com/tmux-plugins/tmux-resurrect.git ~/.tmux_conf/tmux-resurrect
+git clone https://github.com/tmux-plugins/tmux-cpu.git ~/.tmux_conf/tmux-cpu
+git clone https://github.com/tmux-plugins/tmux-sidebar.git ~/.tmux_conf/tmux-sidebar
+git clone https://github.com/MaziyarFG/tmux_config.git ~/.tmux_conf/myconf
